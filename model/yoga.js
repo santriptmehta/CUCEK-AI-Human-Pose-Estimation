4aab5598336bf5096130ab7a6b3c4270364f52ec
Priyanshu <kumarpriyanshu2822@gmail.com>
2023-04-14 01:47:48 +0530

    final pose

--- model/yoga.js
+++ model/yoga.js
@@ -3,7 +3,7 @@ let video;
 let poseNet;
 let pose;
 let skeleton;
-let numOutputs = 8;
+let numOutputs = 10;
 
 let brain;
 
@@ -17,6 +17,8 @@ let triangleAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
 let reverseWarriorAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
 let seatedTwistAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
 let sidePlankAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
+let extendedAngleAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
+let lowLungeRevolvedAngleTarget = [177,138,141,95,177,81,160,175,165,21,167,50,163,84];
 
 let targetArray;
 let label = "";
@@ -104,9 +106,12 @@ function setTargetArray() {
     targetArray = seatedTwistAngleTarget;
   } else if(currentPose === 'Side-Plank') {
     targetArray = sidePlankAngleTarget;
+  } else if(currentPose === 'Extended-Angle') {
+    targetArray = extendedAngleAngleTarget;
+  } else if(currentPose === 'Low-Lunge-Revolved') {
+    targetArray = lowLungeRevolvedAngleTarget;
   }
 }
-
 function loadUserTrainer() {
   // pick model based on user selection
   let pose_name = currentPose.toLowerCase();
